0998b85f62009bbc1f5622bd9b73e01afecba14c
vquynh <ng.viet.quynh@gmail.com>
2025-09-15 22:35:39 +0200

    add VITE_API_URL

--- frontend/src/api/goalService.ts
+++ frontend/src/api/goalService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Goal, Action, ApiError } from '../types';
 
-const API_URL = 'http://localhost:5678';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5678';
 
 // Configure axios instance
 const api = axios.create({
